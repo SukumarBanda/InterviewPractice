1bfd233ef08a17a8de06bfa437e178185e2af06e
SukumarBanda <sukumarbanda@gmail.com>
2022-08-27 14:00:46 +0530

    added second line comment

--- src/com/prep/java/Occurance.java
+++ src/com/prep/java/Occurance.java
@@ -6,6 +6,7 @@ public class Occurance {
 
 	public static void main(String[] args) {
 		//Main method of occurance
+		//Second line comment
 		String str = "sukumar";
 		char [] ch = str.toCharArray();
 		
